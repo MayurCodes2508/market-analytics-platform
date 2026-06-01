292f55f9f41bba16e87bc3221b45d15c5d98bc82
StevenCodes67 <stevenmayurreddy@gmail.com>
2026-06-01 06:05:24 +0530

    8th attempt

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,6 +14,42 @@ resource "google_artifact_registry_repository" "market_analytics_platform_repo" 
   }
 }
 
+resource "google_iam_workload_identity_pool" "github_pool" {
+  workload_identity_pool_id = "github-actions-pool"
+  display_name              = "GitHub Actions Pool"
+  description               = "Identity pool for market analytics CI pipeline"
+}
+
+resource "google_iam_workload_identity_pool_provider" "github_provider" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-provider"
+  display_name                       = "GitHub Actions Provider"
+
+  attribute_mapping = {
+    "google.subject"       = "assertion.subject"
+    "attribute.repository" = "assertion.repository"
+    "attribute.owner"      = "assertion.repository_owner"
+  }
+
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+    attribute_condition = "assertion.repository_owner == 'MayurCodes2508'"
+}
+
+resource "google_service_account_iam_member" "wif_binding" {
+  service_account_id = "projects/instant-medium-491107-t6/serviceAccounts/github-workflows@instant-medium-491107-t6.iam.gserviceaccount.com"
+  role               = "roles/iam.workloadIdentityUser"
+  member = "principalSet://iam.googleapis.com/projects/144449440045/locations/global/workloadIdentityPools/github-actions-pool/attribute.repository/MayurCodes2508/market-analytics-platform"
+  
+  depends_on = [ 
+    google_iam_workload_identity_pool.github_pool,
+    google_iam_workload_identity_pool_provider.github_provider
+   ]
+}
+
+
+
 
 ###################################
 # DEV
@@ -74,21 +110,6 @@ resource "google_cloud_run_v2_job" "dev_market_analytics_platform_run" {
         }
       }
       service_account = "development-cloud-resources-jo@instant-medium-491107-t6.iam.gserviceaccount.com"
-    }
-  }
-}
-
-resource "google_cloud_run_v2_job" "dev_dbt_project_run" {
-  name = "dev-dbt-project-run"
-  location = "asia-south1"
-  template {
-    template {
-      containers {
-        image = ""
-        args = [
-          
-        ]
-      }
     }
   }
 }
