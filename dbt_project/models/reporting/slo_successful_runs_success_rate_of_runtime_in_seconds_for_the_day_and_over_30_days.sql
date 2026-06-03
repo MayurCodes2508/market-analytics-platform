6e2fd117314e3fd18827ef9b4ee17bca33b1db13
StevenCodes67 <stevenmayurreddy@gmail.com>
2026-06-03 15:20:48 +0530

    refactored reporting models, fixed the slo metrics and their breach thresholds

---
+++ dbt_project/models/reporting/slo_successful_runs_success_rate_of_runtime_in_seconds_for_the_day_and_over_30_days.sql
@@ -0,0 +1,51 @@
+WITH base AS (
+SELECT pipeline_name,
+       DATE(created_at) AS created_at,
+       TIMESTAMP_DIFF(end_time, start_time, SECOND) AS runtime
+
+FROM `instant-medium-491107-t6.prod_metadata_staging.stg_pipeline_runs`
+
+WHERE status = 'SUCCESS'
+),
+
+runtime_breach_threshold_flag AS (
+SELECT pipeline_name,
+       created_at,
+       runtime,
+       COALESCE(runtime > 15, TRUE) is_runtime_threshold_breached
+
+FROM base
+),
+
+success_and_records_count AS (
+SELECT pipeline_name,
+       created_at,
+       COUNTIF(is_runtime_threshold_breached IS false) AS success_count,
+       COUNT(*) AS records_count
+
+FROM runtime_breach_threshold_flag
+
+GROUP BY 1, 2
+),
+
+slo_metrics_calculations AS (
+SELECT pipeline_name,
+       created_at,
+       SAFE_DIVIDE(success_count, records_count) * 100 AS success_rate_for_the_day,
+       SAFE_DIVIDE(
+        SUM(success_count) OVER(PARTITION BY pipeline_name ORDER BY created_at ROWS BETWEEN 29 PRECEDING AND CURRENT ROW),
+        SUM(records_count) OVER(PARTITION BY pipeline_name ORDER BY created_at ROWS BETWEEN 29 PRECEDING AND CURRENT ROW)
+       ) * 100 AS success_rate_over_30_days
+
+FROM success_and_records_count
+)
+
+SELECT pipeline_name,
+       created_at,
+       success_rate_for_the_day,
+       COALESCE(success_rate_for_the_day < 95.00, TRUE) AS is_slo_threshold_for_the_day_breached,
+       success_rate_over_30_days,
+       COALESCE(success_rate_over_30_days < 99.00, TRUE) AS is_slo_threshold_over_30_days_breached
+
+FROM slo_metrics_calculations
+
