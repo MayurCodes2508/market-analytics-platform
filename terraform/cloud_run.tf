4f8d8c083e6eb96ed6dfc50e0c65195546a37283
MayurCodes2508 <allamayurreddy@gmail.com>
2026-07-05 06:30:28 +0000

    Added: PROD Cloud Run Job For Running Metadata System

--- terraform/cloud_run.tf
+++ terraform/cloud_run.tf
@@ -208,3 +208,35 @@ resource "google_cloud_run_v2_job" "prod_pipeline_run" {
     }
   }
 }
+
+resource "google_cloud_run_v2_job" "prod_metadata_system_run" {
+  name = "prod-metadata-system-run"
+  location = "asia-south1"
+  deletion_protection = false
+  template {
+    template {
+      containers {
+        image = "asia-south1-docker.pkg.dev/instant-medium-491107-t6/market-analytics-platform-repository/metadata-job:latest"
+        command = [ "bash", "-c" ]
+        args = [ 
+          "python -u -m orchestrator.orchestrator"
+         ]
+         env {
+          name = "ENV"
+          value = "PROD"
+          
+         }
+         env {
+          name = "NEON_DB_URL"
+          value_source {
+            secret_key_ref {
+              secret  = "prod-market-analytics-platform-neon-db-url-secret"
+              version = "1"
+            }
+          }
+         }
+      }
+      service_account = "production-cloud-resources-469@instant-medium-491107-t6.iam.gserviceaccount.com"
+    }
+  }
+}
