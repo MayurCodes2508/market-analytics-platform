ac719a06ffb29af436de0ce32d03d3b866f34bcc
StevenCodes67 <stevenmayurreddy@gmail.com>
2026-07-16 12:40:13 +0530

    Refactor SLO success rate models and tests; streamline data tests and add new consistency checks

--- dbt_transformations/tests/observability/slo_successful_runs_is_slo_threshold_over_30_days_breached_consistent_with_success_rate_over_30_days.sql
+++ dbt_transformations/tests/observability/slo_successful_runs_is_slo_threshold_over_30_days_breached_consistent_with_success_rate_over_30_days.sql
@@ -10,6 +10,6 @@ SELECT  is_slo_threshold_over_30_days_breached
 
 FROM {{ ref('slo_successful_runs_success_rate_for_the_day_and_over_30_days') }}
 
-WHERE   (is_slo_threshold_over_30_days_breached IS FALSE AND success_rate_over_30_days < 0.95) OR
-        (is_slo_threshold_over_30_days_breached IS TRUE AND success_rate_over_30_days >= 0.95)
+WHERE   (is_slo_threshold_over_30_days_breached IS FALSE AND success_rate_over_30_days < 0.99) OR
+        (is_slo_threshold_over_30_days_breached IS TRUE AND success_rate_over_30_days >= 0.99)
 
---
+++ dbt_transformations/tests/observability/slo_successful_runs_runtime_in_seconds_is_slo_threshold_over_30_days_breached_is_boolean.sql
@@ -0,0 +1,13 @@
+{{
+  config(
+    tags=['prod']
+    )
+}}
+
+
+
+SELECT is_slo_threshold_over_30_days_breached
+
+FROM {{ ref('slo_successful_runs_success_rate_of_runtime_in_seconds_for_the_day_and_over_30_days') }}
+
+WHERE is_slo_threshold_over_30_days_breached NOT IN (TRUE, FALSE)
